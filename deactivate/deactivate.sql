bcd024e2d4def62e317cfe6d7a69e12849665fcd
Mauricio <mauricio@vauxoo.com>
2018-04-09 17:42:00 -0400

    add sql

--- deactivate/deactivate.sql
+++ deactivate/deactivate.sql
@@ -1,1 +1,3 @@
 select * from res_users;
+UPDATE ir_mail_server SET active = False, smtp_user = 'user', smtp_pass = '123456';
+UPDATE ir_config_parameter SET value = '123456' WHERE key = 'web.base.url';
