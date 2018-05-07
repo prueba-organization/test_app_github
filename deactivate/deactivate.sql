471bcefe33108d05cf743ae8c9d1055cbeaebf81
Mauricio <mauricio@vauxoo.com>
2018-05-07 17:57:41 -0400

    dummy

--- deactivate/deactivate.sql
+++ deactivate/deactivate.sql
@@ -1,4 +1,2 @@
 select * from res_users;
-UPDATE ir_mail_server SET active = False, smtp_user = 'user', smtp_pass = '123456';
-UPDATE ir_config_parameter SET value = '123456' WHERE key = 'web.base.url';
 select * from ir_model where model = '{{ model  }}';
